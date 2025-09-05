406e42b702c8de87b4783bfe95dbe4f4a659895c
shuraze <126870263+shuraze@users.noreply.github.com>
2025-09-05 11:59:22 +0530

    Update tasks.tf

--- tasks.tf
+++ tasks.tf
@@ -9,12 +9,9 @@
 # templates.
 
 locals {
-  create_default_policy = length(var.tasks_policy_ids) == 0
-  tasks_policy_ids      = local.create_default_policy ? consul_acl_policy.tasks[*].id : var.tasks_policy_ids
-
-  # tasks_role_prefix ensures that roles and binding rules are created with
-  # compatible names. A mismatch can cause login requests to return 403 errors.
-  tasks_role_prefix = "nomad-tasks"
+  role_policies_id = {for role_name, policy_names in var.role_policies : 
+                          "${role_name}" => [for policy in consul_acl_policy.tasks : policy.id if contains(policy_names, policy.name)]
+                          if length(policy_names) == length([for policy in consul_acl_policy.tasks : policy.id if contains(policy_names, policy.name)])}
 }
 
 # consul_acl_binding_rule.tasks binds consul_acl_auth_method.nomad to a role
@@ -24,9 +21,10 @@ locals {
 # Refer to Consul's documentation on binding rules for more information.
 # https://developer.hashicorp.com/consul/docs/security/acl/auth-methods#binding-rules
 resource "consul_acl_binding_rule" "tasks" {
+  for_each = var.binding_rules
   auth_method = consul_acl_auth_method.nomad.name
-  description = "Binding rule for Nomad tasks"
-  bind_type   = "role"
+  description = each.value.description
+  bind_type   = each.value.bind_type
 
   # bind_name must match the name of an ACL role to apply to tokens. You may
   # reference values from the ClaimMappings configured in the auth method to
@@ -34,11 +32,12 @@ resource "consul_acl_binding_rule" "tasks" {
   #
   # Refer to Consul's documentation on claim mappings for more information.
   # https://developer.hashicorp.com/consul/docs/security/acl/auth-methods/jwt#trusted-identity-attributes-via-claim-mappings
-  bind_name = "${local.tasks_role_prefix}-$${value.nomad_namespace}"
+  # bind_name = "${local.bind_name_prefix}-$${value.nomad_namespace}-$${value.nomad_task}-${local.bind_name_suffix}"
+  bind_name = each.value.bind_name
 
   # selector ensures this binding rule is only applied to workload identities
   # for tasks, not services.
-  selector = "\"nomad_service\" not in value"
+  selector = each.value.selector
 }
 
 
@@ -48,38 +47,25 @@ resource "consul_acl_binding_rule" "tasks" {
 # Refer to Consul's documentation on ACL roles for more information.
 # https://developer.hashicorp.com/consul/docs/security/acl/acl-roles
 resource "consul_acl_role" "tasks" {
-
   # As an example, this module creates different roles for each Nomad namespace
   # to illustrate the use of claim mappings attributes, but this can be
   # adjusted as needed in a real cluster.
-  for_each = toset(var.nomad_namespaces)
+  for_each = local.role_policies_id
 
   # The role name must match the value set in the binding rule bind_name
   # attributes.
-  name = "${local.tasks_role_prefix}-${each.key}"
+  name       = each.key
 
   description = "ACL role for Nomad tasks in the ${each.key} Nomad namespace"
-  policies    = local.tasks_policy_ids
+  policies    = each.value
 }
 
 # consul_acl_policy.tasks is a sample ACL policy that grants tokens read access
 # to Consul's service catalog and KV storage.
-#
-# This is the policy used in consul_acl_role.tasks if the variable
-# tasks_policy_ids is not set.
 resource "consul_acl_policy" "tasks" {
-  count = local.create_default_policy ? 1 : 0
-
-  name        = var.tasks_default_policy_name
+ for_each = var.policies
+  name        = each.key
   description = "ACL policy used by Nomad tasks"
 
-  rules = <<EOF
-key_prefix "" {
-  policy = "read"
-}
-
-service_prefix "" {
-  policy = "read"
-}
-EOF
+  rules = each.value
 }
